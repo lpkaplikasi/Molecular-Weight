5bd3e5339f77b95f910862c221a40657cb6815be
lpkaplikasi <168545102+lpkaplikasi@users.noreply.github.com>
2024-05-09 15:05:22 +0700

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ berat = {'H': 1.007, 'He': 4.002, 'Li': 6.94, 'Be': 9.012, 'B': 10.81, 'C': 12.0
 # Menampilkan judul aplikasi
 st.title("Penghitung Berat Unsur Kimia")
 
+ Menampilkan logo aplikasi
+st.image("c:\Users\USER\Downloads\logo aplikasi.jpg",caption='ini background awal')
 
 # Mengambil input berupa unsur kimia dan jumlah mol
 unsur = st.text_input("Masukkan unsur kimia (misal: H):")
